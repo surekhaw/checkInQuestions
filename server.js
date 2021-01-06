2cc05c206674d7f8ea9438701e608b0fbb1f58cc
surekha <s.kaniget@gmail.com>
2021-01-05 17:37:17 -0800

    add backend

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const Datastore = require('nedb');
+const nodemon = require('nodemon');
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+let dbIndexes = [];
+database.find({}, (err, data) => {
+    for(var i = 0; i < data.length; i++) {
+        dbIndexes.push(i);
+    }    
+});
+
+
+// User gets question from database
+app.get('/api', (req, res) => {
+    database.find({}, (err, data) => {  
+        if(err) {
+            res.end();
+            return;
+        } else {
+            let availableRecordIndex = Math.floor(Math.random() * dbIndexes.length); // random index number from checkInQuestions array
+            let randomRecordIndex = dbIndexes[availableRecordIndex];
+            if(dbIndexes.length > 0) {
+                res.json(data[randomRecordIndex]);
+                dbIndexes.splice(availableRecordIndex, 1);
+            } else {
+                res.json({available: 'none', text: 'All currently available check-in questions have been displayed.'})
+            }
+        };
+    });
+});
+
+//User posts check-in questions to database
+app.post('/api', (req, res) => {
+    database.insert(req.body);
+    res.json({
+        status: 'success'
+    })
+    //console.log(database);
+});
